2763ad5bbe0f1ef1559187afad4e079d8eacc8f2
sfederella <santiagoefederella@gmail.com>
2019-09-12 18:25:01 -0300

    Add build

---
+++ client/build/precache-manifest.5a8e72fab80c5e71aaa7c8e79ff247ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83446d910bf9fe8c2071d53018306274",
+    "url": "/index.html"
+  },
+  {
+    "revision": "718f82e501dde2716648",
+    "url": "/static/css/2.e3e156e5.chunk.css"
+  },
+  {
+    "revision": "cc66fbeb433b4197270c",
+    "url": "/static/css/main.4632d80e.chunk.css"
+  },
+  {
+    "revision": "718f82e501dde2716648",
+    "url": "/static/js/2.14e5b3f1.chunk.js"
+  },
+  {
+    "revision": "cc66fbeb433b4197270c",
+    "url": "/static/js/main.c3e32058.chunk.js"
+  },
+  {
+    "revision": "4bc67c488da8d284ef86",
+    "url": "/static/js/runtime~main.254d7f1a.js"
+  },
+  {
+    "revision": "d961fdfabbe512d2675de09af09f598b",
+    "url": "/static/media/jsoneditor-icons.d961fdfa.svg"
+  }
+]);
