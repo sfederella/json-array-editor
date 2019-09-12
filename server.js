059ececd64adb8d727e791078b5a05e156012a28
sfederella <santiagoefederella@gmail.com>
2019-09-12 18:20:57 -0300

    Build productive version

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const api = require('./src/api')
+const path = require('path');
+const favicon = require('express-favicon');
 
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(favicon(__dirname + '/client/build/favicon.ico'));
 app.use('/api', api);
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => {
-  res.send("Hello!");
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
